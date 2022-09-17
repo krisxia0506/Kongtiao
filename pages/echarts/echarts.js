a757d7a98bea83bbb0346647e1375430f8171bd7
“krisxia0506” <xiajiayi0506@gmail.com>
2022-09-17 16:46:18 +0800

    重大更新
    1.增加底部导航栏
    2.增加echarts图表
    3.增加我的页面

--- pages/echarts/echarts.js
+++ pages/echarts/echarts.js
@@ -1,112 +1,66 @@
 import * as echarts from '../../ec-canvas/echarts';
 
-let chart = null;
+const app = getApp();
 
 function initChart(canvas, width, height, dpr) {
-  chart = echarts.init(canvas, null, {
+  const chart = echarts.init(canvas, null, {
     width: width,
-    height: height,
+    height: 500,
     devicePixelRatio: dpr // new
   });
   canvas.setChart(chart);
 
   var option = {
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-        type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-      },
-      confine: true
+    title: {
+      text: '用电量曲线',
+      left: 'center'
     },
     legend: {
-      data: ['热度', '正面', '负面']
+      data: ['A', 'B', 'C'],
+      top: 25,
+      left: 'center',
+      backgroundColor: 'white',
+      z: 100
     },
     grid: {
-      left: 20,
-      right: 20,
-      bottom: 15,
-      top: 40,
       containLabel: true
     },
-    xAxis: [
-      {
-        type: 'value',
-        axisLine: {
-          lineStyle: {
-            color: '#999'
-          }
-        },
-        axisLabel: {
-          color: '#666'
+    tooltip: {
+      show: true,
+      trigger: 'axis'
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+      // show: false
+    },
+    yAxis: {
+      x: 'center',
+      type: 'value',
+      splitLine: {
+        lineStyle: {
+          type: 'dashed'
         }
       }
-    ],
-    yAxis: [
-      {
-        type: 'category',
-        axisTick: { show: false },
-        data: ['汽车之家', '今日头条', '百度贴吧', '一点资讯', '微信', '微博', '知乎'],
-        axisLine: {
-          lineStyle: {
-            color: '#999'
-          }
-        },
-        axisLabel: {
-          color: '#666'
-        }
-      }
-    ],
-    series: [
-      {
-        name: '热度',
-        type: 'bar',
-        label: {
-          normal: {
-            show: true,
-            position: 'inside'
-          }
-        },
-        data: [300, 270, 340, 344, 300, 320, 310],
-        itemStyle: {
-          // emphasis: {
-          //   color: '#37a2da'
-          // }
-        }
-      },
-      {
-        name: '正面',
-        type: 'bar',
-        stack: '总量',
-        label: {
-          normal: {
-            show: true
-          }
-        },
-        data: [120, 102, 141, 174, 190, 250, 220],
-        itemStyle: {
-          // emphasis: {
-          //   color: '#32c5e9'
-          // }
-        }
-      },
-      {
-        name: '负面',
-        type: 'bar',
-        stack: '总量',
-        label: {
-          normal: {
-            show: true,
-            position: 'left'
-          }
-        },
-        data: [-20, -32, -21, -34, -90, -130, -110],
-        itemStyle: {
-          // emphasis: {
-          //   color: '#67e0e3'
-          // }
-        }
-      }
-    ]
+      // show: false
+    },
+    series: [{
+      name: 'A',
+      type: 'line',
+      smooth: true,
+      data: [18, 36, 65, 30, 78, 40, 33]
+    }, {
+      name: 'B',
+      type: 'line',
+      smooth: true,
+      data: [12, 50, 51, 35, 70, 30, 20]
+    }, {
+      name: 'C',
+      type: 'line',
+      smooth: true,
+      data: [10, 30, 31, 50, 40, 20, 10]
+    }]
   };
 
   chart.setOption(option);
@@ -117,7 +71,7 @@ Page({
   onShareAppMessage: function (res) {
     return {
       title: 'ECharts 可以在微信小程序中使用啦！',
-      path: 'pages/echarts/echarts',
+      path: '/pages/index/index',
       success: function () { },
       fail: function () { }
     }
@@ -129,9 +83,5 @@ Page({
   },
 
   onReady() {
-    setTimeout(function () {
-      // 获取 chart 实例的方式
-      // console.log(chart)
-    }, 2000);
   }
 });
---
+++ pages/me/me.js
@@ -0,0 +1,90 @@
+ Page({
+     data: {
+         hiddenBlean1: true,
+         hiddenBlean2: false,
+         userInfo: '', //用户信息
+         nickName: '', //用户姓名
+         avatarUrl: '', //用户头像地址
+         m: 0, //用户的登录状态
+     },
+     //退出登录
+     loginOut() {
+         this.setData({
+             userInfo: '',
+             hiddenBlean1: true,
+             hiddenBlean2: false,
+             m: 0
+         })
+         wx.request({
+             url: '',
+             method: 'POST',
+             header: {
+                 'Content-Type': 'application/json'
+             },
+             success: res => {
+                 console.log(res)
+             }
+         })
+         wx.setStorageSync('user', null),
+             wx.showToast({
+                 title: '已退出登录',
+             })
+     },
+
+     //获取用户信息
+     getUersProfile: function () {
+         if (this.data.m == 0) {
+             wx.getUserProfile({
+                 desc: '登陆后使用全部功能',
+                 success: (res) => {
+                     let user = res.userInfo
+                     //console.log('获取成功', res)
+                     wx.setStorage({
+                         data: res.userInfo,
+                         key: 'userInfo',
+                     });
+
+                     wx.login({ //获取code向后端发送并请求token
+                         success(res) {
+                             //console.log(res.code)
+                             let code = res.code
+                             wx.request({
+                                 url: '' + res.code,
+                                 method: 'POST',
+                                 header: {
+                                     'Content-Type': 'application/json'
+                                 },
+                                 success: res => {
+                                     //console.log(res)
+                                     //console.log(res.data.data.token)
+                                     wx.setStorageSync('token', res.data.data.token)
+                                 }
+                             })
+                         }
+                     });
+                     //console.log('用户信息', res.userInfo),
+                         //console.log(res.userInfo.nickName),
+                         this.setData({
+                             m: 1,
+                             nickName: res.userInfo.nickName,
+                             avatarUrl: res.userInfo.avatarUrl,
+                             hiddenBlean2: true,
+                             hiddenBlean1: false
+                         })
+                     wx.showToast({
+                         title: '登陆成功',
+                     })
+                     wx.hideToast()
+                     this.onShow();
+                 },
+                 fail: (res) => {
+                     //console.log('授权失败', res)
+                 }
+             })
+         } else {
+             wx.showToast({
+                 title: '您已经登录了',
+             })
+         }
+     }
+ })
